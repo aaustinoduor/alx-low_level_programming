5b07f396eba4f61d3970004334481b95ace1631c
aaustinoduor <aaustinoduorr@gmail.com>
2023-07-14 18:05:17 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry
+ *
+ * Return Always 0 (success)
+ */
+
+int main(void)
+{
+int lowerCase = 'a';
+while(lowerCase <= 'z')
+{
+if (lowerCase == 'a' || lowerCase 'q')
+{
+lowerCase += 1;
+}
+else
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+}
+putchar('\n');
+return (0);
+}
