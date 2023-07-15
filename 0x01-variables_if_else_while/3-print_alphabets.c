0a7d36cc93b6d174eda0f5ac2974fda6d2d14bf8
aaustinoduor <aaustinoduorr@gmail.com>
2023-07-15 12:22:27 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 
 /**
  * Main - Entry
@@ -10,10 +9,16 @@
 int main(void)
 {
 int LowerCase = 'a';
+int UpperCase = 'A';
 while(LowerCase <= 'z')
 {
 putchar(LowerCase);
 LowerCase += 1;
+}
+while(UpperCase <= 'z')
+{
+putchar(UpperCase);
+UpperCase += 1;
 }
 putchar('\n');
 return (0);
