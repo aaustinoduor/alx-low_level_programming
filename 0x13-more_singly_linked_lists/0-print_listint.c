d3e4507238a4ab4aaeeb8c6681fe852bfa189a42
aaustinoduor <aaustinoduor>
2023-08-29 08:20:23 -0700

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all elements of a linked list
+ * print_listint - prints all elements of a linked list
  * @h: linked list of type listint_t to print
  *
- * Return: Number of nodes
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
