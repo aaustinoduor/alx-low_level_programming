288179977657dcfdebf2c4f06e631b6cf2183116
aastinoduor <aastinoduorr@gmail.com>
2023-09-29 11:16:04 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,13 +8,12 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int k;
 	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	for (k = 0; b[k]; k++)
+	for (int k = 0; b[k]; k++)
 	{
 		if (b[k] < '0' || b[k] > '1')
 			return (0);
